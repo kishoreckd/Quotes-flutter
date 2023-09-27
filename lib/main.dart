4db9a19c6e4ce9a0cd6f910129604963cd41353a
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-27 12:19:25 +0530

    quote_card class has been added seperately  for understanding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quotes/quote.dart';
+import 'package:quotes/quote_card.dart';
+
 
 void main() {
   runApp(const MaterialApp(
@@ -41,36 +43,4 @@ class _QuoteListState extends State<QuoteList> {
   }
 }
 
-class QuoteCard extends StatelessWidget {
-  const QuoteCard({
-    super.key,
-    required this.quotes,
-  });
 
-  final Quote quotes;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                quotes.text,
-                style: const TextStyle(fontSize: 18.0, color: Colors.grey),
-              ),
-              const SizedBox(
-                height: 6.0,
-              ),
-              Text(
-                quotes.author,
-                style: const TextStyle(fontSize: 18.0, color: Colors.grey),
-              ),
-            ],
-          ),
-        ));
-  }
-}
