d28cb6da3fb6b20c67b29eb52eed20c638884692
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-27 10:20:54 +0530

    The project started and quotes as been shown on the Ui

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'The truth is rarely pure and never simple.',
+    'The Hardest Hoice Requires the Strongest Will.',
+    'We won Sir.'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((e) => Text(e)).toList(),
+      ),
+    );
+  }
+}
