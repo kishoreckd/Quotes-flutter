2d2747b2255097a1f5e2c2e151d339a0e53862a6
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-27 12:04:17 +0530

    quotes added using widgets

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,31 @@ class _QuoteListState extends State<QuoteList> {
         author: 'oscar'),
     Quote(text: 'We won Sir..', author: 'oscar Issac'),
   ];
+
+  Widget quoteTemplate(Quote quotes) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                quotes.text,
+                style: const TextStyle(fontSize: 18.0, color: Colors.grey),
+              ),
+              const SizedBox(
+                height: 6.0,
+              ),
+              Text(
+                quotes.author,
+                style: const TextStyle(fontSize: 18.0, color: Colors.grey),
+              ),
+            ],
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +57,9 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((e) => Text('${e.text} - ${e.author}')).toList(),
+        children: quotes.map((e) => quoteTemplate(e)).toList(),
+// listcomprehension
+        // children: quotes.map((e) => Text('${e.text} - ${e.author}')).toList(),
       ),
     );
   }
